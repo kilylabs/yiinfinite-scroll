1a36ab2bf9944b7d9f5abc25975111f4fc97d13c
root <root@www.kovea.ru>
2018-06-07 17:44:44 -0400

    bugfix

--- YiinfiniteScroller.php
+++ YiinfiniteScroller.php
@@ -65,7 +65,7 @@ class YiinfiniteScroller extends CBasePager {
     }
 
     public function registerClientScript() {
-        $url = CHtml::asset(Yii::getPathOfAlias('ext.yiinfinite-scroll.assets').'/jquery.infinitescroll.min.js');
+        $url = CHtml::asset(Yii::getPathOfAlias('vendor.kilylabs.yiinfinite-scroll.assets').'/jquery.infinitescroll.min.js');
         Yii::app()->clientScript->registerScriptFile($url);
     }
 
